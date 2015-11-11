58b38da57a6117da12505ac1e4ff4c7cea481819
Matei Oprea <eu@opreamatei.ro>
2015-11-11 21:44:18 +0200

    add initial module

---
+++ index.js
@@ -0,0 +1,27 @@
+var request = require('request');
+
+module.exports = function(key){
+
+    return {
+        recognize: function () {
+            var args = arguments;
+            var type = args[0],
+                image = args[1];
+            var payload = {
+                image: image
+            };
+            request({
+                url: "https://api.projectoxford.ai/emotion/v1.0/recognize",
+                method: "POST",
+                json: true,
+                body: payload,
+                headers: {
+                    "content-type" : "application/json",
+                    "Ocp-Apim-Subscription-Key" : key
+                }
+            }, function (error, response, body){
+                console.log(response);
+            });
+        }
+    }
+};
